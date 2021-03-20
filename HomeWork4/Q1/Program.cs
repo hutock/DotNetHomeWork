62c50343653f53f71c9d4e5285955c842da03924
谢之 黎 <1159198565@qq.com>
2021-03-20 12:06:16 +0800

    第四次作业

---
+++ HomeWork4/Q1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> G= new GenericList<int>();
+            for (int i = 0; i < 10;  i++)
+            {
+                G.Add(i);
+            }
+            G.ForEach(a => Console.Write($"{a.data} "));
+
+            Console.WriteLine();
+
+            int max = int.MinValue;
+            G.ForEach(a => max = (a.data <= max) ? max : a.data);
+            Console.WriteLine($"最大值是{max}");
+
+            int min = int.MaxValue;
+            G.ForEach(a => min = (a.data >= min) ? min : a.data);
+            Console.WriteLine($"最小值是{min}");
+
+            int sum = 0;
+            G.ForEach(a => sum += a.data);
+            Console.WriteLine($"和是{sum}");
+        }
+        
+    }
+
+    public class GenericList<T>
+    {
+        public Node<T> head;
+        public Node<T> tail;
+
+        public GenericList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void Add(T data)
+        {
+
+            Node<T> node = new Node<T>(data);
+            if (head == null)
+            {
+                head = node;
+                tail = head;
+            }
+            else
+            {
+                tail.next = node;
+                tail = node;
+            }
+        }
+
+        public void ForEach(Action<Node<T>> action)
+        {
+            Node<T> elem = head;
+            while (elem != null)
+            {
+                action(elem);
+                elem = elem.next;
+            }
+        }
+    }
+
+    public class Node<T>
+    {
+        public T data { get; set; }
+        public Node<T> next { get; set; }
+
+        public Node(T data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+}
---
+++ HomeWork4/Q2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClock c = new myClock();
+            c.setAlarm(12, 2, 0);
+            c.theClock.TimeStartGoing();
+        }
+    }
+}
+
+public class Time
+{
+    public int hour { get; set; }
+    public int minute { get; set; }
+    public int second { get; set; }
+
+    public void timeGo()
+    {
+        if(second<59)
+        {
+            second++;
+        }
+        else
+        {
+            second = 0;
+            if (minute < 59)
+                minute++;
+            else
+            {
+                minute = 0;
+                if (hour < 23)
+                    hour++;
+                else
+                    hour = 0;
+                
+            }
+        }
+    }
+}
+
+public delegate void TimeRecord(Time time);
+
+public class Clock
+{
+    public event TimeRecord Tick;
+    public event TimeRecord Alarm;
+
+    public void TimeStartGoing()
+    {
+        Time now = new Time()
+        {
+            hour = System.DateTime.Now.Hour,
+            minute = System.DateTime.Now.Minute,
+            second = System.DateTime.Now.Second
+        };
+        Tick(now);
+    }
+
+    public void TimeOut(Time time)
+    {
+        Alarm(time);
+    }
+}
+
+public class myClock
+{
+    public Clock theClock = new Clock();
+    public Time myAlarm = new Time() { hour = 0,minute = 0, second = 0 };
+
+    public myClock()
+    {
+        theClock.Tick += TimeInGoing;
+        theClock.Alarm += AlarmTimeOut;
+    }
+
+    public void setAlarm(int hour, int minute, int second)
+    {
+        this.myAlarm.hour = hour;
+        this.myAlarm.minute = minute;
+        this.myAlarm.second = second;
+    }
+
+    public void TimeInGoing(Time time)
+    {
+        while(true)
+        {
+
+            theClock.TimeOut(time);
+            Console.WriteLine($"现在是{time.hour}点{time.minute}分{time.second}秒");
+            time.timeGo();
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+
+    public void AlarmTimeOut(Time time)
+    {
+        if (time.hour == myAlarm.hour && time.minute == myAlarm.minute && time.second == myAlarm.second)
+            Console.WriteLine("闹钟响了！！");
+    }
+}
